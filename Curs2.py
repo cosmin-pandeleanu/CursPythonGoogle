c18ab6417bc6d8935a11d60542a0ef24995cb133
Cosmin Pandeleanu <cosmin.pandeleanu@yahoo.com>
2021-11-04 19:32:40 +0200

    Instructiuni

---
+++ Curs2.py
@@ -0,0 +1,45 @@
+# my_var = 5
+# msg = "Nicio conditie nu este respectata"
+# msg1 = 0
+# if my_var < 6:
+#     msg , msg1 = "Set instructiuni 1",my_var
+# elif my_var > 10:
+#     msg = "Set instructiuni 2"
+# print(msg, msg1)
+#
+#
+# a = 1
+# b = 2
+# x = 1 if a > b else -1
+# var = sintaxa_then if conditie else sintaxa_else
+# print(x)
+
+# while True:
+#     print("Set instr.")
+#     break
+
+# for i in range(10):
+#     print(f'Set instructiuni {i}')
+
+var = 'Ana are mere'
+# for i in var:
+#     print(i)
+
+# lista = [i for i in var]
+# print(lista)
+# for item, value in enumerate(var):
+#     print(f"{item} => {value}")
+
+
+# dict = {"key1" : "value1" , "key2":"value2"}
+# for item in dict.items():
+#     print(item)
+#     item0, item1 = item
+#     print(item0, item1)
+# for key, value in dict.items():
+#     print(key, value)
+
+# var = (1,) #tuple
+
+# var = "Ana 'are' mere"
+# print(var)
